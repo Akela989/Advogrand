623b52ab9c38f5b3eaa6764e97d957d62ceb1585
Akela989 <antrapenov@yandex.ru>
2019-09-30 19:17:15 +0500

    Final Page

--- src/js/main.js
+++ src/js/main.js
@@ -108,3 +108,38 @@ window.onscroll = function showTopSide() {
       topSide.classList.remove('header_top_fixed');
     }
 }
+
+
+
+$('.call_button').click(function() { 
+  
+  $('.js-overlay-campaign').fadeIn();
+  $('.js-overlay-campaign').addClass('disabled');
+});
+
+$('.md_adapt').click(function() { 
+  
+  $('.js-overlay-campaign').fadeIn();
+  $('.js-overlay-campaign').addClass('disabled');
+});
+
+// закрыть на крестик
+$('.js-close-campaign').click(function() { 
+  $('.js-overlay-campaign').fadeOut();
+  
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) { 
+  var popup = $('.js-popup-campaign');
+  if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+    $('.js-overlay-campaign').fadeOut();
+    
+  }
+});
+
+
+
+$(function($){
+  $('[name="phone"]').mask("+9(999) 999-9999");
+});
